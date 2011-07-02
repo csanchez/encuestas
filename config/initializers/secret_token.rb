c09a3d3932ca17cef740e2ba07235cd688d46181
Carlos Sanchez Perales <caer@ciencias.unam.mx>
2011-07-02 18:45:05 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Encuestas::Application.config.secret_token = '856ec9bbcd85421afdb70b67cfc2cbeee03c8a8d62e30aa0989788f0f8e8c853a235b747900d8dd944ad39075ebd8aac9a16c4df7a61aef939f399070b3dd53d'
